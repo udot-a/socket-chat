212a2acd0800a234af98cc013ac5223227caf5a1
Udot Andrii <udot.dron@gmail.com>
2020-10-13 20:49:16 +0300

    herokuTrySuper

---
+++ build/precache-manifest.b0418a374d5884f7cd96afeb59fdc09f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a22e5e4acd68317b19e3cce8224a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70f6fb6bba0b60e496d8",
+    "url": "/static/css/main.eadce8ea.chunk.css"
+  },
+  {
+    "revision": "0a87b75bd36eef9b5efc",
+    "url": "/static/js/2.31997b1f.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.31997b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f6fb6bba0b60e496d8",
+    "url": "/static/js/main.d3daaf5c.chunk.js"
+  },
+  {
+    "revision": "5ea7b155b9eed09194f1",
+    "url": "/static/js/runtime-main.1f6bf8ab.js"
+  }
+]);
